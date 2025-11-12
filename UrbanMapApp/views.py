99cabe409cd0430b2bfb6eac11d91760872d4338
Kasimis <petekasimis224@gmail.com>
2025-11-12 19:38:52 +0200

    completed report submission, added reports categories

--- UrbanMapApp/views.py
+++ UrbanMapApp/views.py
@@ -1,28 +1,42 @@
+# UrbanMapApp/views.py
+
 from django.shortcuts import render, redirect
 from django.conf import settings
 from .models import Report, Category
 from django.contrib.auth.decorators import login_required
 
+
 def home(request):
 
-
-    if request.method == "POST":
+    if request.method == 'POST' and request.user.is_authenticated:
+        # Get data from the form
         title = request.POST.get('title')
+        description = request.POST.get('description')
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-        if request.user.is_authenticated:
+        category_id = request.POST.get('category')
+        photo = request.FILES.get('photo')
+        category = Category.objects.get(id=category_id)
+
+
+
+        if category and photo:
             new_report = Report(
                 user=request.user,
                 title=title,
-                description="default description",
-                photo='report_photos/default.jpg',
+                description=description,
+                photo=photo,
                 latitude=latitude,
                 longitude=longitude,
+                category=category
             )
             new_report.save()
-        return redirect("home")
+
+        return redirect('home')
     else:
+        categories = Category.objects.all()
         context = {
             'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+            'categories': categories,
         }
-    return render(request, 'home.html', context)
+        return render(request, 'home.html', context)
