416a2864ccc9780b5ec22049fd3b19e2a14357dd
Kasimis <petekasimis224@gmail.com>
2025-11-12 19:20:35 +0200

    imported models to admin tab, added report submit functionality

--- UrbanMapApp/admin.py
+++ UrbanMapApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Report, Vote
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description', 'user__username')
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'report')
--- UrbanMapApp/views.py
+++ UrbanMapApp/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+from .models import Report, Category
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    context = {
-        'page_title': 'Χάρτης Αστικών Προβλημάτων',
-        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
-    }
+
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        if request.user.is_authenticated:
+            new_report = Report(
+                user=request.user,
+                title=title,
+                description="default description",
+                photo='report_photos/default.jpg',
+                latitude=latitude,
+                longitude=longitude,
+            )
+            new_report.save()
+        return redirect("home")
+    else:
+        context = {
+            'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+        }
     return render(request, 'home.html', context)
